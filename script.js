8bf14101bd98c197f681ef03cc11b04b75c8ce86
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-04-07 05:47:24 +0000

    finalizing 1.5

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(function() {
+    
+    var toggled = false;
     $('.collapser').click(function(){
         var disp = $(this).html()
         var part = disp.substring(0, 4);
@@ -7,6 +9,26 @@ $(function() {
             $(this).html("Show " + rem);
         }else{
             $(this).html("Hide " + rem);
+        }
+    });
+    
+    $('.nav-item').click(function(){
+        $("#nav-1").collapse('hide');
+    });
+    
+    $(document).scroll(function() {
+        var scroll = $(window).scrollTop();
+        console.log(scroll)
+        if (scroll >= 650) {
+            if (toggled==false){
+                $(".navbar").toggleClass('bg-info')
+                toggled = true;
+            }
+        }else{
+            if (toggled==true){
+                toggled = false;
+                $(".navbar").toggleClass('bg-info')
+            }
         }
     });
 });
