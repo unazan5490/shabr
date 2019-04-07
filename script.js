b40e7ebafea89acb1b655c9de3a7b99eae753914
ZhiYuan Chen <zhiyuanc1718@hstat.org>
2019-04-07 21:35:44 +0000

    optimizing js

--- script.js
+++ script.js
@@ -19,26 +19,19 @@ $(function() {
         }, 1000);
         $("#nav-1").collapse('hide');
     });
-    
-    //initiate pos to 0 if no value yet (new to site)
-    if (localStorage.getItem("pos") == null) {
-        localStorage.setItem('pos', $(window).scrollTop());
-    }
-    
-    //as page (re)loads, compare value to see if navbar needs to display
-    if (localStorage.getItem('pos') >= $(window).height() / 3) {
-        $(".navbar").addClass('bg-primary')
-    }
-    
-    //when user is scrolling through the page, check their new position
-    $(document).scroll(function() {
-        localStorage.setItem('pos', $(window).scrollTop());
-        var scroll = localStorage.getItem('pos');
+
+    function display() {
+        var scroll = $(window).scrollTop()
         if (scroll >= $(window).height() / 3) {
             $(".navbar").addClass('bg-primary')
         }
         else {
             $(".navbar").removeClass('bg-primary')
         }
+    }
+
+    display();
+    $(document).scroll(function() {
+        display();
     });
 });
